4f995d5ca979b6f10b7ecbb19c815c50194592e5
Matthieu Muffato <muffato@ebi.ac.uk>
2019-07-04 21:48:39 +0100

    Bumped the versions to force an update

--- compara.rb
+++ compara.rb
@@ -7,7 +7,7 @@ class Compara < Formula
   url 'file://'+File.expand_path(__FILE__)
   desc 'Dependencies for the Compara formula'
   sha256 Digest::SHA256.file(File.expand_path(__FILE__)).hexdigest
-  version '13'
+  version '14'
 
   depends_on 'ensembl/external/exonerate24'
   depends_on 'ensembl/external/infernal'
